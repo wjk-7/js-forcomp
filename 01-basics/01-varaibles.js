7e8a3971f246da9821b3810a02ebabb30976075b
wjk-7 <forcomputer162@gmail.com>
2025-01-21 09:34:15 +0000

    data and variable

---
+++ 01-basics/01-varaibles.js
@@ -0,0 +1,6 @@
+let name = "wahaj"
+let id = "767187568"
+let city = "karachi"
+let state = true
+let home;
+console.table([name, id, city,state,home])
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+let name = "wahaj"
+let age = 20
+let address;
+let state = null
+console.table([typeof(name),typeof(6),typeof(address),typeof(state)])
