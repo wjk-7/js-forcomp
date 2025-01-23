b47445999ba175621831a3c64214fc86a63cd958
wjk-7 <forcomputer162@gmail.com>
2025-01-23 09:29:42 +0000

    day 3

---
+++ 01-basics/05-string.js
@@ -0,0 +1,10 @@
+let sentence = "this is a red fix and green"
+// console.log(`this function makes ${sentence.at(-4)} and defines something`)
+// console.log(`the char at  4 is ${sentence.charAt(4)}`)
+let stri1 = "hi"
+let stri2 = "world"
+// console.log(stri1.concat(" " ,stri2))
+// console.log(stri1.endsWith("i",2))
+let word = "fox"
+// console.log(`this sentence has ${sentence.includes(word) ? 'yes' : 'no'} in it` )
+console.log(sentence.replaceAll(" ","-"))
