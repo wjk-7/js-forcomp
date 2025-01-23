92d2ed7790a908e00b4ba51afa4ceace059ac91d
wjk-7 <forcomputer162@gmail.com>
2025-01-23 09:57:05 +0000

    day 3

---
+++ 01-basics/06-maths.js
@@ -0,0 +1,9 @@
+let num = 100.65536
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(3))
+// console.log(Math.round(100.5)
+let min = 10
+let max = 20
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
