4435ba531a71221ccdc4faf1a4efa74dd7ec284f
wjk-7 <forcomputer162@gmail.com>
2025-01-25 09:22:14 +0000

    day 5

---
+++ 02-basics/01-array.js
@@ -0,0 +1,24 @@
+const arr = ["dr.strange","spiderman","shaktikapoor"]
+const arr2 = ["w","a","h","a","j"]
+// console.log(arr.concat(arr2))
+// const new_arr2 = (arr2.pop())
+// console.log(arr2.slice(0,3))
+// console.log(arr2)
+// console.log(arr2.splice(0,3))
+// console.log(arr2)
+ const arr3 = ["i","j"]
+// arr2.push(arr3)
+// console.log(arr2);
+// console.log([...arr,...arr2])
+// console.log(Array.isArray(arr3));
+// console.log(Array.from("khan"));
+let a = [1,234,2553,32894, "wahaj"]
+let b = [743,647,77,'khan']
+console.log(Array.of(a,b))
+console.log([...a,...b]);
+
+
+
+
+
+
