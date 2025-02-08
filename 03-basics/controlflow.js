e3e56a65d83a8202c8f89cf47d749dd4ffaf400d
wjk-7 <forcomputer162@gmail.com>
2025-02-08 08:39:24 +0000

    day 13

---
+++ 03-basics/controlflow.js
@@ -0,0 +1,28 @@
+// const empthyObj = {}
+// if (Object.keys(empthyObj.length === 0)) {
+//     console.log("this is empthy");
+//     console.log(
+//         Object.keys(empthyObj));
+// }
+// const empthyArr = []
+// if (empthyArr.length === 0) {
+//     console.log("this is empthy");
+// }
+const month = 4
+switch(month){
+    case 1 :
+        console.log("jan")
+        break;
+    case 2 :
+        console.log("feb");
+        break;
+    case 3 :
+        console.log("march");
+        break;
+        default:
+            console.log("not in calender");
+            
+           
+        
+
+}
