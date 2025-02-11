7ecf48e26e94b053b5253409c8708cc7582567e7
wjk-7 <forcomputer162@gmail.com>
2025-02-11 10:34:16 +0000

    day 15

---
+++ 03-basics/loop2.js
@@ -0,0 +1,21 @@
+const myarr = ["flash","superman","batman"]
+const myobj = {
+    name : "wahaj",
+    age : 22,
+    state : "karachi"
+}
+for (const key in myobj) {
+    console.log(`${key} is ${myobj[key]}`);
+    
+
+
+}
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+    // console.log(key, ':-', value);
+    
+// }
---
+++ 03-basics/loops.js
@@ -0,0 +1,15 @@
+for (let i = 2; i <=2; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    // console.log(i + '*' + j + ' = ' + i*j );
+   }
+}
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
